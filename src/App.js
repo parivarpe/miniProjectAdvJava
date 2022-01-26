d2b8b5487fdd0fb5d13bbe056922ba450ec86802
parivarpe <parivarpe01@gmail.com>
2022-01-26 14:09:49 +0530

    routerintro

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Page1></Page1>}></Route>
+
+        <Route path="/page2" element={<Page2></Page2>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+//Routers
+function Page1() {
+  return (
     <div>
-      <Hello></Hello>
+      <h1> Page1 </h1>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident qui
+        consequuntur obcaecati blanditiis repellendus, molestias dolorem eum
+        delectus numquam labore. Nostrum soluta, accusamus hic earum
+        praesentium, blanditiis repellat iste sapiente officiis nam dolore.
+        Fugit facere voluptas accusamus quas hic commodi, modi dicta vel iste
+        quaerat, reprehenderit aperiam delectus recusandae laboriosam? Voluptas
+        quod ducimus aperiam! Nesciunt vitae minima dolores ipsam suscipit
+        veritatis sit ratione numquam aut, saepe nostrum nulla quis labore
+        tenetur officia sapiente expedita neque consectetur delectus fuga
+        incidunt unde iure porro? Deserunt et veritatis laboriosam quam
+        perspiciatis tempora culpa, molestias voluptatem fugiat illo laudantium
+        debitis, in ipsa qui quaerat?{" "}
+      </p>
     </div>
   );
 }
 
-//Hooks and State
-function Hello() {
-  let counter = 1000; //simple variable declaration
-
-  //RHS :: Special function :: startwith, prefix, useState, useEffect
-  //React Hooks -
-  //useState : It works with Dom internally. it take care of DOM manipulation
-
-  let [username, setUserName] = useState(""); // declaration using useState
-  let [password, setPass] = useState("");
-  const handleUserName = (e) => {
-    const newUname = e.target.value;
-    setUserName(newUname);
-  };
-
-  const handlepass = (e) => {
-    const newPass = e.target.value;
-    setPass(newPass);
-  };
-
+function Page2() {
   return (
     <div>
-      <input type="text" value={username} onChange={handleUserName}></input>
-      <input type="password" value={password} onChange={handlepass}></input>
-      <h1>{username}</h1>
-      <h1>{password}</h1>
+      <h1> Page2 </h1>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident qui
+        consequuntur obcaecati blanditiis repellendus, molestias dolorem eum
+        delectus numquam labore. Nostrum soluta, accusamus hic earum
+        praesentium, blanditiis repellat iste sapiente officiis nam dolore.
+        Fugit facere voluptas accusamus quas hic commodi, modi dicta vel iste
+        quaerat, reprehenderit aperiam delectus recusandae laboriosam? Voluptas
+        quod ducimus aperiam! Nesciunt vitae minima dolores ipsam suscipit
+        veritatis sit ratione numquam aut, saepe nostrum nulla quis labore
+        tenetur officia sapiente expedita neque consectetur delectus fuga
+        incidunt unde iure porro? Deserunt et veritatis laboriosam quam
+        perspiciatis tempora culpa, molestias voluptatem fugiat illo laudantium
+        debitis, in ipsa qui quaerat?{" "}
+      </p>
     </div>
   );
 }
