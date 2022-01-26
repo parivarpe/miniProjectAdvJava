711dc9a59f9d95a6c289f822ef9ccf4fadadcf33
parivarpe <parivarpe01@gmail.com>
2022-01-26 12:15:51 +0530

    useStaet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { useState } from "react";
+
 export default function App() {
   return (
     <div>
       <Hello></Hello>
-      <Hello id="1" name="Pari"></Hello>
     </div>
   );
 }
 
-//function with parameter restructuring
-function Hello({ name, id }) {
-  return <h1>Hello {name}</h1>;
+//Hooks and State
+function Hello() {
+  let counter = 1000; //simple variable declaration
+
+  //RHS :: Special function :: startwith, prefix, useState, useEffect
+  //React Hooks -
+  //useState : It works with Dom internally. it take care of DOM manipulation
+
+  let [counter1, setCounter] = useState(1); // declaration using useState
+
+  const like = () => {
+    const newCount = counter1 + 1;
+    setCounter(newCount);
+  };
+
+  return (
+    <div>
+      <h1>
+        Counter : {counter1}
+        <button onClick={like}> Like</button>
+      </h1>
+    </div>
+  );
 }
